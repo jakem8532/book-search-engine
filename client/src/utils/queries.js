fe63f203c0550eae363d02aca7b6f8221f281edf
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-10-09 12:03:45 -0400

    added mutations and queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client'
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                title
+                link
+            }
+        }
+    }
+`
