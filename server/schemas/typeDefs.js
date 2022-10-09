9dac2105608c53fdaf2b8b919648a2962927ae8b
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-10-09 11:50:18 -0400

    added typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = reqyure('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int 
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        title: String
+    }
+
+    type Query {
+        me: User
+    }
+
+    input savedBookInput {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!) : Auth 
+        login(email: String!, password: String!): Auth
+        saveBook(book: savedBookInput): User
+        deleteBook(bookId: String!) : User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`
+
+module.exports = typeDefs
